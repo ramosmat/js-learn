dd281506278f0b10998859f19a6793ddf780e277
Matheus <matheusramosbi.13@gmail.com>
2023-12-27 23:39:51 -0300

    forth commit

---
+++ 04-Condições/aula008.js
@@ -0,0 +1,9 @@
+var vel = 40;
+
+if (vel > 100) {
+  console.log("NUH ta com pressa ein");
+} else if (vel > 60 * 1.1) {
+  console.log("Vamo com calllmaaa");
+} else {
+  console.log("Ta tranquilo, só vaai");
+}
