934f35672c78eae085230c19725bbbc3e9d148b0
Matheus <matheusramosbi.13@gmail.com>
2024-03-05 00:35:22 -0300

    add `14-Atribuição&Ternário` folder

---
+++ 14-Atribuição&Ternário/ori-atribuicao.js
@@ -0,0 +1,8 @@
+//Atribuição é uma forma de fazermos operações, mas mais simplificadas
+let numero = 20;
+
+numero += 10; //numero = numero + 10
+numero /= 2; //numero = numero / 2
+numero **= 2; //numero = numero ** 2
+
+console.log(numero);
---
+++ 14-Atribuição&Ternário/ori-ternario.js
@@ -0,0 +1,8 @@
+let idade = 18;
+
+let podeBeber;
+let possuiDiabetes = true;
+
+podeBeber = idade >= 18 && !possuiDiabetes ? "Pode beber" : "Não pode beber";
+
+console.log(podeBeber);
